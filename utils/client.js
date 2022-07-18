18f0c25520a81728e79aaec418e507167de993d7
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-07-18 14:19:18 -0500

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -9,7 +9,7 @@ const clientCredentials = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 if (!firebase.apps.length) {
